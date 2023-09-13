9869738e9759fa9004aebf08fe4f7fa37843505d
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-14 00:32:42 +0530

    fifth

--- DeleteFile.java
+++ DeleteFile.java
@@ -6,5 +6,9 @@ public class DeleteFile {
         File filePath = new File("/home/ajan/Desktop/Ajan");
         boolean delete = filePath.delete();
         System.out.println(delete ? "file deleted":"file not exist");
+
+        File file2 = new File("/home/ajan/Desktop/abc.txt");
+        boolean delete2 = file2.delete();
+        System.out.println(delete2 ? "file deletd":"file not exist");
     }
 }

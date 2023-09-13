62c26ab6af72bd6d9ba6434cddcbae4a68c47bcc
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-14 00:17:16 +0530

    Second

--- CreateFile.java
+++ CreateFile.java
@@ -1,5 +1,15 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CreateFile {
     public static void main(String[] args) {
+
+         File file = new File("/home/ajan/Desktop/Ajan");
+        boolean mkdir = file.mkdir();
+        System.out.println(mkdir?"file created":"file ALREADY EXIST");
+
+        Path PathRef = Paths.get("/home/ajan/Desktop/Ajan new file");
         
     }
 }

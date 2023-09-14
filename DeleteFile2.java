82363e78f65eeed63a3f98469ac2815f70cb3e4b
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-14 10:08:30 +0530

    Completed

--- DeleteFile2.java
+++ DeleteFile2.java
@@ -3,5 +3,15 @@ import java.io.File;
 public class DeleteFile2 {
     public static void main(String[] args) {
         File fileRef = new File("/home/ajan/Desktop/Ajan new file");
+        if(fileRef.isDirectory()){
+            File[] ListFiles = fileRef.listFiles();
+
+            for(File file : ListFiles){
+                file.delete();
+                System.out.println(file + "Deleted");
+            }
+        }
+        fileRef.delete();
+        System.out.println("File Deleted");
     }
 }

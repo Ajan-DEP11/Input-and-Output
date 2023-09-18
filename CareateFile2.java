48f911b09f5343ef09152a39a4cace8353a0df38
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-18 12:49:57 +0530

    second commit

--- CareateFile2.java
+++ CareateFile2.java
@@ -1,5 +1,34 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Path;;
+
 public class CareateFile2 {
     public static void main(String[] args) {
+         File file = new File("Ajan.txt");
+         boolean mkdir = file.mkdir();
+         System.out.println(mkdir?"file created":"Not created");
+
+
+         Path path = Paths.get("Dep11");
+         try{
+            Files.createDirectories(path);
+
+
+         }catch(FileAlreadyExistsException e){
+            System.out.println("file already exist");
+            System.out.println("Path already exist");
+
+         }catch(IOException e){
+            System.out.println("IO already implemented");
+            e.printStackTrace();
+
+         }
+
+
         
     }
 }

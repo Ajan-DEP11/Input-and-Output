f039eb4437ce305422647487de3bd78cfda1757f
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-19 19:35:11 +0530

    fifteenth commit

--- DeleteFile4.java
+++ DeleteFile4.java
@@ -2,12 +2,12 @@ import java.io.File;
 
 public class DeleteFile4 {
     public static void main(String[] args) {
-        File file = new File("Ajan.txt");
+        File file = new File("Ajan");
         if(file.isDirectory()){
             File[] listFiles = file.listFiles();
             for(File file2 : listFiles){
                 file2.delete();
-                System.out.println("File list is deleted");
+                System.out.println(file2 + "File list is deleted");
 
             }
         }
---
+++ FileRead.java
@@ -0,0 +1,5 @@
+public class FileRead {
+    public static void main(String[] args) {
+        
+    }
+}

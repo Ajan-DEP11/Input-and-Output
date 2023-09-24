6ca8ad1b56896882494a3d6eea7688c644b8e96d
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-24 23:49:48 +0530

    last commit

--- WriteFile2.java
+++ WriteFile2.java
@@ -1,5 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class WriteFile2 {
     public static void main(String[] args) {
+
+        try{
         
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("Ijse.txt",false));
+        bufferedWriter.write("Hello world\n");
+        bufferedWriter.write("we are born to code\n");
+        bufferedWriter.append("im a softwere Engineer");
+        bufferedWriter.close();
+
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e);
+
+        }
     }
 }

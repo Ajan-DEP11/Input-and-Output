03c08aa3ca7f31ea62648cf8847c39444944210d
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-24 23:15:11 +0530

    writer second commit

--- WriteFile1.java
+++ WriteFile1.java
@@ -1,5 +1,15 @@
+import java.io.FileWriter;
+
 public class WriteFile1 {
     public static void main(String[] args) {
-        
+        String txt = "My Name is ajan eshwara, i'm a software engineer";
+        try{
+         FileWriter Writer = new FileWriter("Dep11.txt");
+         Writer.write(txt);
+         Writer.close();
+         System.out.println("file write complted");
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 }

9004c0c8399835d37c770a4e950174ad6be3b05d
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-25 01:01:05 +0530

    second

--- FileReader.java
+++ FileReader.java
@@ -1,18 +1,29 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
 
 public class FileReader {
-    public static void main(String[] args) throws Exception{
-        
+    public FileReader(String string) {
+    }
 
-
+    public static void main(String[] args) {
+        File file = new File("read.txt");
         try{
+        FileWriter fileWriter = new FileWriter(file, true);
+        fileWriter.write(" Hello World,we are born to code");
+        System.out.println("Done");
+        fileWriter.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            FileReader fileReader = new FileReader(file);
+            System.out.println((char)fileReader.read());
 
-            FileReader fileReader = new FileReader();
-            
         }catch(Exception e){
             System.out.println(e);
         }
 
-        
+      
     }
 }

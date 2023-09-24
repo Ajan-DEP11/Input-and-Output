80b6f9994e9988350c29003bc8a978f095e9c1f3
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-24 21:06:29 +0530

    first commit

---
+++ FileCreate2.java
@@ -0,0 +1,5 @@
+public class FileCreate2 {
+    public static void main(String[] args) {
+        
+    }
+}
--- FileReader.java
+++ FileReader.java
@@ -1,5 +1,18 @@
+import java.io.File;
+
 public class FileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        
+
+
+        try{
+
+            FileReader fileReader = new FileReader();
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
         
     }
 }

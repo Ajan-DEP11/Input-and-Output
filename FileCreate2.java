a4d1fbb052e86b81193cc8103d2e894037ca7079
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-24 21:18:42 +0530

    second commit

--- FileCreate2.java
+++ FileCreate2.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+
 public class FileCreate2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileAlreadyExistsException{
+
+        File file = new File("ajan.txt");
+        try{
+
+        boolean createNewFile = file.createNewFile();
+        System.out.println("File created");
+        System.out.println(file.createNewFile());
+        System.out.println(file.getPath());
+        System.out.println(file.exists());
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.canExecute());
+        System.out.println(file.canWrite());
+       // } catch(FileAlreadyExistsException e){
+         //   System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+
+
         
     }
 }
